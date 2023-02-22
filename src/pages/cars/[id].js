33d34efcd3a74e1992fd31094cdc3cccac71949b
jon-cron <jonathanacm95@gmail.com>
2023-02-22 10:08:50 -0700

    head note

--- src/pages/cars/[id].js
+++ src/pages/cars/[id].js
@@ -1,6 +1,7 @@
 // NOTE the file name [id].js tells next that this file should render a single subset of cars or single car api/cars/id. the "[id]" work as params to a page
 // NOTE the useRouter lets us use the params above
 import { useRouter } from "next/router.js";
+// NOTE this Head import allows us to import the metadata that we would usually write in the top of an html file
 import Head from "next/head.js";
 
 export default function Car({car}) {
@@ -31,6 +32,16 @@ export async function getStaticProps({params}) {
 
 export async function getStaticPath() {
 
-  
+  const req = await fetch(`http://localhost:3000/${params.id}.json`)
+  const data = await req.json();
+
+  const paths = data.map(car => {
+    return {params: {id: car}}
+  })
+
+  return {
+    paths,
+    fallback: false
+  }
 
 }
